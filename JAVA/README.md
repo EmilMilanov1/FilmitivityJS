86b9b484acf6a105c985220a0520ef3354549485
EmilMilanov1 <emil.milanov.highschool@svetlina.net>
2025-01-21 09:29:46 +0200

    first commit

---
+++ JAVA/README.md
@@ -0,0 +1,18 @@
+# Filmtivity
+
+Filmtivity is a web application that allows users to explore and discover information about movies. This project is powered by [The Movie Database (TMDb) API](https://www.themoviedb.org/documentation/api).
+
+## Features
+
+- **Home Page:** Explore the world of movies with a brief introduction.
+- **All Movies Page:** View a collection of popular movies.
+- **Search Page:** Search for your favorite movies and get detailed information.
+
+## Getting Started
+
+### Prerequisites
+
+Before you begin, ensure you have the following installed:
+
+- Web browser
+- Code editor (e.g., Visual Studio Code, Sublime Text)
---
+++ JAVA/scripts/all.js
@@ -0,0 +1,38 @@
+const API_KEY = "af8dd00a3f4bfd71d94d95695bef6e79";
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetchTopTenMovies();    
+});
+
+function fetchTopTenMovies() {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`)
+        .then(response => response.json())
+        .then(data => {
+            displayMovies(data.results.slice(0, 12));
+        })
+        .catch(error => console.error(error));
+}
+
+function displayMovies(movies) {
+    let movieContainer = document.getElementById('movie-container');
+
+    movieContainer.innerHTML = '';
+
+    movies.forEach(movie => {
+        let movieCard = `
+            <div class="flip flip-vertical">
+                <div class="front">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                </div>
+                <div class="back">
+                    <h2>${movie.title}</h2>
+                    <p>Description: ${movie.overview}</p>
+                    <p>Vote Average: ${movie.vote_average}</p>
+                </div>
+            </div>
+        `;
+
+        movieContainer.innerHTML += movieCard;
+    });
+}
+
